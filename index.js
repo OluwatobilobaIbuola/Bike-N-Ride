c62be5c2a11af8b19766cc4cb77acaa772364791
Oluwatobiloba Ibuola <ibuolatobi@gmail.com>
2022-04-07 21:01:59 +0300

    add better color selection and transition effect

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function boxShadow(x){      
-    x.classList.add("box-shadow");
-}
-function boxShadowOff(x){      
-    x.classList.remove("box-shadow");
-}
+
 function classToggle() {
     document.querySelector(".navbar-item").classList.toggle("navbar-toggleshow")
   }
-  
+
